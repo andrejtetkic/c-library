dfeb4d9919c0cbd89616c2bc0ce71a4162a5c687
Andrej Tetkic <andrej.tetkic@gmail.com>
2024-05-02 01:47:40 +0200

    Cleaned main from testing

--- main.c
+++ main.c
@@ -10,37 +10,9 @@
 
 
 int main(){
-    // initializeTranslationTable();
+    initializeTranslationTable();
 
-    // landingPage();
-
-//    User temp;
-
-//     strcpy(temp.userID, "12345");
-//    strcpy(temp.name, "Name123");
-
-//     DB_insert(UserT, &temp);
-
-   // DB_update("123545", UserT, &temp);
-
-    ComparisonPair compare_pairs[] = {
-        { compareByUserID, "123" }, 
-       // { compareByUserID, "12345" },
-        // { compareBySomeField, "some_value" }
-    };
-
-    int num_found;
-    User* found_users = DB_select(UserT, compare_pairs, sizeof(compare_pairs), &num_found);
-
-    if (found_users != NULL) {
-        printf("Found %d user(s):\n", num_found);
-        for (int i = 0; i < num_found; i++) {
-            printf("User ID: %s\n", found_users[i].userID);
-        }
-        free(found_users);
-    } else {
-        printf("No matching records found.\n");
-    }
+    landingPage();
 
     pressEnter();
     return 0;
